64bfa0d6f0f277289cff98ba8af6c196a51bf6dc
Chris-Fontein <104692148+Chris-Fontein@users.noreply.github.com>
2025-11-23 23:42:39 -0500

    Event performance test case added

---
+++ section7/src/api/Testcase_EventPerformanceAPI.js
@@ -0,0 +1,41 @@
+export const getEventPerformanceData = [
+	{
+		id:"Event 1",
+		rsvpCount: 50,
+		actual: 40
+	},
+	{
+		id:"Event 2",
+		rsvpCount: 100,
+		actual: 75
+	},
+	{
+		id:"Event 3",
+		rsvpCount: 0,
+		actual: 62
+	},
+	{
+		id:"Event 4",
+		rsvpCount: -14,
+		actual: 62
+	},
+	{
+		id:"Event 5",
+		rsvpCount: 73,
+		actual: -30
+	},
+	{
+		id:"Event 6",
+		rsvpCount: 82,
+	},
+	{
+		id:"Event 7",
+		actual: 10,
+	},
+
+	{
+		id:"Event 8",
+		rsvpCount: 60,
+		actual: 0
+	},
+];
