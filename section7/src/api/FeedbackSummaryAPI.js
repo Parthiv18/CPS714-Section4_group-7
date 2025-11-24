4d5f110fb6cf2b15e6c96aa8911a093a83f30644
Chris-Fontein <104692148+Chris-Fontein@users.noreply.github.com>
2025-11-23 19:13:10 -0500

    Update to API inputs
    API inputs are now split into separate entries that are parsed and consolidated for display

--- section7/src/api/FeedbackSummaryAPI.js
+++ section7/src/api/FeedbackSummaryAPI.js
@@ -1,10 +1,68 @@
 export const feedbackSummaryData = {
-  "Event 1": {
-    averageRating: 4.5,
-    comments: [
-      "Great event! Very informative.",
-      "Enjoyed the networking opportunities.",
-      "Excellent speakers and relevant topics.",
-    ],
+  10: {
+    event:"Event 2",
+    comments: "Life changing."
+  },
+  11: {
+    event:"Event 2",
+    rating: 5,
+  },
+  12: {
+    rating: 5,
+  },
+  13: {
+  },
+  6: {
+    event:"",
+    rating: 4.5,
+    comments: "No event specified"
+  },
+  6: {
+    event:"Bad",
+    rating: -1,
+    comments: "Negative Scores are bad"
+  },
+  7: {
+    event:"Bad",
+    rating: 6,
+    comments: "Score above 5"
+  },
+  8: {
+    event:"Bad",
+    comments: "No score given"
+  },
+  0: {
+    event:"Event 1",
+    rating: 4.5,
+    comments: "Enjoyed the networking opportunities."
+  },
+  1: {
+    event:"Event 1",
+    rating: 5,
+    comments: "Great event! Very informative."
+  },
+  2: {
+    event:"Event 1",
+    rating: 3,
+    comments: "Could have been better."
+  },
+  5: {
+    event:"Event 2",
+    rating: 4.5,
+    comments: "Really well done."
+  },
+  3: {
+    event:"Event 1",
+    rating: 0,
+    comments: "What nonsense."
+  },
+  4: {
+    event:"Event 2",
+    rating: 5,
+    comments: "Life changing."
+  },
+  9: {
+    event:"Event 2",
+    rating: 0
   },
 };
--- section7/src/components/FeedbackSummary.js
+++ section7/src/components/FeedbackSummary.js
@@ -5,6 +5,46 @@ import { feedbackSummaryData } from "./../api/FeedbackSummaryAPI";
 const FeedbackSummary = () => {
   const eventRows = Object.entries(feedbackSummaryData);
 
+  var temp_dict = {};
+  var finalRows = [];
+  for(var i in eventRows)
+  {
+    var row = eventRows[i][1];
+
+    if(row.event === undefined || row.event === "" || row.rating === undefined || row.rating < 0 || row.rating > 5)
+    {
+      continue;
+    }
+
+    if(temp_dict[row.event] === undefined)
+    {
+      temp_dict[row.event] = {count:0, rating:0, comments:[]}
+    }
+    temp_dict[row.event].count += 1;
+    temp_dict[row.event].rating += row.rating;
+    
+    if(row.comments === undefined || row.comments === "")
+    {
+      continue;
+    }
+    
+    if(temp_dict[row.event].count === 4)
+    {
+      temp_dict[row.event].comments.push("...");
+    }
+    else if(temp_dict[row.event].count < 4)
+    {
+      temp_dict[row.event].comments.push(row.comments);
+    }
+  }
+
+  for(var i in temp_dict)
+  {
+    finalRows.push([i, {averageRating:(temp_dict[i].rating/temp_dict[i].count).toFixed(2), comments:temp_dict[i].comments}]);
+  }
+
+
+
   return (
     <div className="widget event-performance">
       <h2>Feedback Summary</h2>
@@ -18,7 +58,7 @@ const FeedbackSummary = () => {
           </tr>
         </thead>
         <tbody>
-          {eventRows.map(([eventName, data]) => (
+          {finalRows.map(([eventName, data]) => (
             <tr key={eventName}>
               <td>{eventName}</td>
               <td>
