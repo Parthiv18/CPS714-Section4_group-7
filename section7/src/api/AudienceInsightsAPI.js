c3cf6bb2ce1e90443bf89dd3f87fc1a3d6a76f1b
Chris-Fontein <104692148+Chris-Fontein@users.noreply.github.com>
2025-11-23 21:47:59 -0500

    Modification of Audience insights display and testcase

--- section7/src/api/AudienceInsightsAPI.js
+++ section7/src/api/AudienceInsightsAPI.js
@@ -1,16 +1,56 @@
 export const audienceInsightsData = {
   "Event 1": {
-    colleges: {
-      "Faculty of Engineering and Architectural Science": 40,
-      "Faculty of Arts": 30,
-      "Ted Rogers School of Management": 50,
-    },
-    majors: {
-      "Computer Science": 30,
-      "Business Management": 25,
-      "Mechanical Engineering": 20,
-      History: 15,
-      Sociology: 30,
-    },
+      "Toronto Metropolitan": {
+        "Computer Science": 30,
+        "Business Management": 25
+      },
+      "University of Toronto": {
+        "History":15,
+        "Political Sciene":10,
+        "Computer Science": 13,
+      },
+      "York University": {
+        "Mechanical Engineering": 20,
+        History: 15,
+        Sociology: 30,
+      },
+      "Mount Allison": {
+        "Computer Science": -14,
+        Accounting: 15,
+        Chemistry: 30,
+      },
   },
-};
+
+  "Event 2": {
+      "Univerity of Alberta": {
+        "Fine Arts": 10,
+        "English": 17
+      },
+      "University of Toronto": {
+      },
+      "York University": {
+        "Mechanical Engineering": 0,
+        History: 15,
+        Sociology: 30,
+      },
+  },
+
+
+  "Event 3": {
+  },
+
+  "Event 4": {
+      "Univerity of Alberta": {
+      },
+      "University of Toronto": {
+      },
+      "York University": {
+      },
+  },
+
+    "Event 5": {
+      "Univerity of Alberta": {
+        "Computer Science":55
+      }
+  },
+}
--- section7/src/components/AudienceInsights.js
+++ section7/src/components/AudienceInsights.js
@@ -11,6 +11,82 @@ const AudienceInsights = () => {
       .join(", ");
   };
 
+  var temp_dict = {};
+  var finalRows = [];
+
+  for(var i in eventRows)
+  {
+    var event = eventRows[i][0];
+    var entry = eventRows[i][1];
+
+    if(temp_dict[event] === undefined)
+    {
+      temp_dict[event] = {};
+    }
+
+    for(var c in entry)
+    {
+      var count = 0;
+      var college = entry[c];
+
+      if(temp_dict[event][c] === undefined)
+      {
+        temp_dict[event][c] = {count:0, majors:[]}
+      }
+      else
+      {
+        count = temp_dict[event][c].count;
+      }
+
+      var col_info = temp_dict[event][c].majors;
+
+      
+      for(var m in college)
+      {
+        if(college[m] <= 0)
+          continue;
+        count += college[m];
+        if(col_info[m] === undefined)
+        {
+          col_info[m] = college[m];
+        }
+        else
+        {
+          col_info[m] += college[m];
+        }
+      }
+      temp_dict[event][c].count = count;
+    }
+  }
+
+  for(i in temp_dict)
+  {
+    var event_used = false;
+
+    for(var c in temp_dict[i])
+    {
+      var college_used = false;
+      for(var m in temp_dict[i][c].majors)
+      {
+        if(event_used === false)
+        {
+          finalRows.push([i, c + " - " + temp_dict[i][c].count, m + " - " + temp_dict[i][c].majors[m]]);
+          event_used = true;
+          college_used = true;
+        }
+        else if(college_used === false)
+        {
+          finalRows.push(["", c + " - " + temp_dict[i][c].count, m + " - " + temp_dict[i][c].majors[m]]);
+          college_used = true;
+        }
+        else
+        {
+          finalRows.push(["", "", m + " - " + temp_dict[i][c].majors[m]]);
+        }
+      }
+    }
+  }
+
   return (
     <div className="widget audience-insights">
       <h2>Event Performance Insights</h2>
@@ -24,11 +100,12 @@ const AudienceInsights = () => {
           </tr>
         </thead>
         <tbody>
-          {eventRows.map(([eventName, eventDetails]) => (
-            <tr key={eventName}>
-              <td>{eventName}</td>
-              <td>{formatDataList(eventDetails.colleges)}</td>
-              <td>{formatDataList(eventDetails.majors)}</td>
+          {finalRows.map(([event, college, major]) => (
+            <tr key={event}>
+              <td>{event}</td>
+              <td>{college}</td>
+              <td>{major}</td>
+
             </tr>
           ))}
         </tbody>
